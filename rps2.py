a23485cc0de0b68375b7bfe967efc2477995c87e
8904878PAS <35676715+8904878PAS@users.noreply.github.com>
2018-02-14 10:29:44 +0530

    Add files via upload

---
+++ rps2.py
@@ -0,0 +1,60 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: /home/cs2017a205/prathik/rps21.py =================
+r, p, sr
+You win! r smashes Scissors
+r, p, ss
+You win! s cut Scissors
+r, p, sr
+You win! r smashes Scissors
+r, p, sp
+You lose! Scissors cut p
+r, p, ss
+You win! s cut Scissors
+r, p, sr
+You win! r smashes Scissors
+r, p, sp
+You lose! Scissors cut p
+r, p, sr
+You win! r smashes Scissors
+r, p, ss
+You win! s cut Scissors
+r, p, sp
+You lose! Scissors cut p
+r, p, sr
+You win! r smashes Scissors
+r, p, ss
+You win! s cut Scissors
+r, p, sp
+You lose! Scissors cut p
+r, p, sr
+You win! r smashes Scissors
+r, p, ss
+You win! s cut Scissors
+r, p, sp
+You lose! Scissors cut p
+r, p, sr
+You win! r smashes Scissors
+r, p, sw
+That's not a valid play. Check your spelling!
+r, p, s
+================= RESTART: /home/cs2017a205/prathik/rps21.py =================
+r, p, sp
+You win! p covers Paper
+r, p, s
+================= RESTART: /home/cs2017a205/prathik/rps21.py =================
+r, p, sr
+You win! r smashes Scissors
+r, p, sr
+You win! r smashes Scissors
+r, p, sr
+You win! r smashes Scissors
+r, p, sr
+You win! r smashes Scissors
+r, p, sr
+You win! r smashes Scissors
+r, p, sp
+You lose! Scissors cut p
+r, p, s
